7b610cc93bd17eccad7d0126118f457f8028b33d
Tatsuya Kitabatake <sissoko@Tatsuya-no-MacBook-Pro.local>
2015-06-06 00:48:10 +0900

    create prototype. #1

---
+++ ssfind.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+
+#define MAX_DEPTH 1
+
+void searchRecursive(char *path, int depth);
+
+int main(int argc, char *argv[])
+{
+	
+	char *searchPattern;
+	
+	if(argc < 2){
+		printf("please specify file search pattern.\n");
+		return 0;
+	}
+	
+	searchPattern = argv[1];
+	
+	searchRecursive("./", 0);
+	
+	return 0;
+}
+
+void searchRecursive(char *path, int depth){
+	
+	if(depth > MAX_DEPTH){
+		return;
+	}
+	
+	DIR *dir;
+	
+	struct dirent *dp;
+	dir=opendir(path);
+	
+	char childDirName[256];
+	
+	
+	for(dp = readdir(dir); dp != NULL; dp = readdir(dir)){
+		
+		if(strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0){
+			continue;
+		}
+			
+		if(dp->d_type == DT_REG){
+			//TODO filtering
+			
+			for(int i = 0; i < depth; i++){
+				printf("  ");
+			}
+			
+			printf("%s%s\n",path, dp->d_name);
+		}
+		else if(dp->d_type == 4){
+			
+			strcpy(childDirName, path);
+			strcat(childDirName, dp->d_name);
+			strcat(childDirName, "/");
+			searchRecursive(childDirName, depth + 1);
+		}
+	}
+	
+	closedir(dir);
+	
+}
